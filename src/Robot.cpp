c7740898627260b32903b67ab49dd9da76826874
James Ward <j.ward@acfr.usyd.edu.au>
2015-02-07 21:53:56 +1100

    Create header for Robot class.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,86 +1,76 @@
-#include "WPILib.h"
-#include "CommandBase.h"
+#include "Robot.h"
+#include <lib-4774/Functions.h>
 
-#define _PI 3.14159
-
-class Robot: public IterativeRobot
+void Robot::RobotInit()
 {
-private:
-	Command *autonomousCommand;
-	LiveWindow *lw;
-	AnalogInput *ir;
-	bool irTripped = false;
+    CommandBase::init();
+    lw = LiveWindow::GetInstance();
+    ir = new AnalogInput(0);
+}
 
-	void RobotInit()
-	{
-		CommandBase::init();
-		lw = LiveWindow::GetInstance();
-		ir = new AnalogInput(0);
-	}
-	
-	void DisabledPeriodic()
-	{
-		Scheduler::GetInstance()->Run();
-		PutDashboard();
+void Robot::DisabledPeriodic()
+{
+    Scheduler::GetInstance()->Run();
+    PutDashboard();
 
-		CommandBase::elevator->endSwitchTripped = false;
-		CommandBase::elevator->readySwitchTopTripped = false;
-		CommandBase::elevator->readySwitchBottomTripped = false;
-		irTripped = false;
+    CommandBase::elevator->endSwitchTripped = false;
+    CommandBase::elevator->readySwitchTopTripped = false;
+    CommandBase::elevator->readySwitchBottomTripped = false;
+    irTripped = false;
 
-	}
+}
 
-	void AutonomousInit()
-	{
-		if (autonomousCommand != NULL)
-			autonomousCommand->Start();
-	}
+void Robot::AutonomousInit()
+{
+    if (autonomousCommand != NULL)
+        autonomousCommand->Start();
+}
 
-	void AutonomousPeriodic()
-	{
-		Scheduler::GetInstance()->Run();
-		PutDashboard();
-	}
+void Robot::AutonomousPeriodic()
+{
+    Scheduler::GetInstance()->Run();
+    PutDashboard();
+}
 
-	void TeleopInit()
-	{
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to 
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		if (autonomousCommand != NULL)
-			autonomousCommand->Cancel();
-	}
+void Robot::TeleopInit()
+{
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (autonomousCommand != NULL)
+        autonomousCommand->Cancel();
+}
 
-	void TeleopPeriodic()
-	{
-		Scheduler::GetInstance()->Run();
-		PutDashboard();
-		SmartDashboard::PutNumber("Encoder Cm: ", CommandBase::elevator->getEncoder());
-	}
+void Robot::TeleopPeriodic()
+{
+    Scheduler::GetInstance()->Run();
+    PutDashboard();
+    SmartDashboard::PutNumber("Encoder Cm: ", CommandBase::elevator->getEncoder());
+}
 
-	void TestPeriodic()
-	{
-		lw->Run();
-	}
+void Robot::TestPeriodic()
+{
+    lw->Run();
+}
 
-	void PutDashboard() {
-	    SmartDashboard::PutData(CommandBase::imu);
+void Robot::PutDashboard() {
+    SmartDashboard::PutData(CommandBase::imu);
 
-	    SmartDashboard::PutNumber("RollDeg", (CommandBase::imu->GetRoll() * 180/_PI));
-	    SmartDashboard::PutNumber("PitchDeg", (CommandBase::imu->GetPitch() * 180/_PI));
-	    SmartDashboard::PutNumber("YawDeg", (CommandBase::imu->GetYaw() * 180/_PI));
+    SmartDashboard::PutNumber("RollDeg", lib4774::r2d(CommandBase::imu->GetRoll()));
+    SmartDashboard::PutNumber("PitchDeg", lib4774::r2d(CommandBase::imu->GetPitch() ));
+    SmartDashboard::PutNumber("YawDeg", lib4774::r2d(CommandBase::imu->GetYaw()));
 
-	    SmartDashboard::PutBoolean("End Switch Tripped", CommandBase::elevator->endSwitchTripped);
-	    SmartDashboard::PutBoolean("Ready Switch Top Tripped", CommandBase::elevator->readySwitchTopTripped);
-	    SmartDashboard::PutBoolean("Ready Switch Bottom Tripped", CommandBase::elevator->readySwitchBottomTripped);
-	    if(ir->GetValue() < 500) {
-	        irTripped = true;
-	    }
-	    SmartDashboard::PutBoolean("Ir Trippped: ", irTripped);
-	    SmartDashboard::PutNumber("Ir Sensor", ir->GetValue());
-	}
-};
+    SmartDashboard::PutBoolean("End Switch Tripped", CommandBase::elevator->endSwitchTripped);
+    SmartDashboard::PutBoolean("Ready Switch Top Tripped", CommandBase::elevator->readySwitchTopTripped);
+    SmartDashboard::PutBoolean("Ready Switch Bottom Tripped", CommandBase::elevator->readySwitchBottomTripped);
+    if(ir->GetValue() < 500) {
+        irTripped = true;
+    }
+    SmartDashboard::PutBoolean("Ir Trippped: ", irTripped);
+    SmartDashboard::PutNumber("Ir Sensor", ir->GetValue());
+}
+
 
 START_ROBOT_CLASS(Robot);
 
---
+++ src/Robot.h
@@ -0,0 +1,27 @@
+#ifndef Robot_h
+#define Robot_h
+
+#include "WPILib.h"
+#include "CommandBase.h"
+
+class Robot: public IterativeRobot
+{
+private:
+    Command *autonomousCommand;
+    LiveWindow *lw;
+    AnalogInput *ir;
+    bool irTripped = false;
+
+    void PutDashboard();
+
+public:
+    void RobotInit();
+    void DisabledPeriodic();
+    void AutonomousInit();
+    void AutonomousPeriodic();
+    void TeleopInit();
+    void TeleopPeriodic();
+    void TestPeriodic();
+};
+
+#endif
